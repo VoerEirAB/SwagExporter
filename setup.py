9b357fb1f1f11de8c50b74515b4d217637425de8
anchal39 <anchal@voereir.com>
2022-12-21 11:04:29 +0530

    [Delivers #183336988] Reformat setup.py (#3)

--- setup.py
+++ setup.py
@@ -17,25 +17,35 @@
 # under the License.                                           #
 ################################################################
 
-from setuptools import setup, find_packages
+import json
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+current_path = os.path.abspath(os.path.dirname(__file__))
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+version_file = os.path.join(current_path, 'swag_exporter', 'setup_info.json')
+with open(version_file) as f:
+    pkg_info = json.loads(f.read())
 
 setup(
-    name='swag_export',
-    version='0.0.1',
-    packages=find_packages(),
-    url='https://voereir.com',
-    author='VoerEir AB',
-    author_email='info@voereir.com',
-    description='Swag Exporter for exporting swagger spec json as html to pdf',
+    name=pkg_info["name"],
+    version=pkg_info["version"],
+    license=pkg_info['license'],
+    packages=find_packages(exclude=pkg_info['exclude_folders']),
+    url=pkg_info["url"],
+    author=pkg_info["author"],
+    author_email=pkg_info["author_email"],
+    description=pkg_info["description"],
+    long_description=pkg_info['long_description'],
     install_requires=required,
     include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'VoerEir_Engines = swag.main:main'
-        ]
-    }
+    classifiers=pkg_info['classifiers'],
+    platforms=pkg_info['platforms'],
+    python_requires=pkg_info['python_requires'],
+    package_data=pkg_info['package_data'],
+    entry_points=pkg_info['entry_points'],
 )
